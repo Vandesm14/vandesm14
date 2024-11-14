abef82361014a82f0dc7625d3eb44e515a8217e2
Shane <vandesm14@gmail.com>
2024-11-14 10:52:05 -0500

    update build sorting and tldr

--- src/build.ts
+++ src/build.ts
@@ -7,21 +7,38 @@ let main = Deno.readTextFileSync('src/index.html');
 const tldr = Deno.readTextFileSync('src/content/tldr.md');
 const testimony = Deno.readTextFileSync('src/content/testimony.md');
 
-const project_list = Deno.readDirSync('src/content/projects');
+const projects_unsorted = Deno.readDirSync('src/content/projects');
+const project_sorted = Array.from(projects_unsorted);
+project_sorted.sort((a, b) => {
+  const a_name = a.name;
+  const b_name = b.name;
+  if (a_name < b_name) {
+    return -1;
+  }
+  if (a_name > b_name) {
+    return 1;
+  }
+  return 0;
+});
+
 let project_strings: string = '';
-for (const project of project_list) {
+for (const project of project_sorted) {
   const name = project.name;
   const content = Deno.readTextFileSync(`src/content/projects/${name}`);
 
-  project_strings += `${parse(content)}\n`;
+  project_strings += `<section class="project">\n${parse(
+    content
+  )}\n</section>\n`;
 }
 
 const year = new Date().getFullYear();
 const experience = year - 2017;
+const rust = year - 2022;
 const job = year - 2021;
 
 main = main.replace('$xp', experience.toString());
 main = main.replace('$year', year.toString());
+main = main.replace('$rust', rust.toString());
 
 main = main.replace('{tldr}', parse(tldr));
 main = main.replace(
--- src/content/tldr.md
+++ src/content/tldr.md
@@ -1,5 +1,6 @@
-I am a developer (TypeScript & Rust), artist, music artist, photography-artist, and multimedia enthusiast (artist). I program, mostly, though.
+I am a developer (Rust & TypeScript), music artist, and multimedia enthusiast. I have been progremming in TypeScript for $xp years and switched to loving Rust about $rust years ago (it happens). Most of my projects start from what I call _"dumb ideas"_ that are usually ambitious in the beginning, but can end up working spectacularly.
 
 - [GitHub](https://github.com/vandesm14)
-- [Twitter](https://twitter.com/vandesm14)
+- [Bluesky](https://bsky.app/profile/thedevbird.bsky.social)
+- [Blog](https://blog.thedevbird.com)
 - [LinkedIn](https://www.linkedin.com/in/shane-vandegrift-769696192/)
